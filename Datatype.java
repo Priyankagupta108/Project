afeecc72f7ee8ac2240d62aa24983adc0c90f242
Priyankagupta108 <115006768+Priyankagupta108@users.noreply.github.com>
2025-06-06 02:39:40 -0700

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,20 @@
+package First;
+
+public class Datatype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int maths=65;
+ int science =55;
+ int english=72;
+ int total;
+ 
+ total=maths+science+english;
+ double average=total/3;
+ System.out.println("total="+total);
+ System.out.println("Average="+average);
+ int average2=total/3;
+ System.out.println("Average2="+average2);
+	}
+
+}
