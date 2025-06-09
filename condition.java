f4a90efdcf7df2e45a590b4020d2fbe97715a2f8
Priyankagupta108 <115006768+Priyankagupta108@users.noreply.github.com>
2025-06-09 16:58:05 +0530

    Add files via upload

---
+++ condition.java
@@ -0,0 +1,36 @@
+package First;
+
+public class condition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int maths=65;
+		 int science =55;
+		 int english=72;
+		 int total;
+		 
+		 total=maths+science+english;
+		 int average=total/3;
+		 {
+		 
+		 if(90>=100)
+		 {
+		 System.out.println("A+ excellent");
+		 }
+		 else if(75>=89)
+		 {
+		 System.out.println("A excellent");
+		 }
+		 else if(60>=74)
+		 {
+		 System.out.println("B");
+		 }
+		 else {
+			 System.out.println("Keep improving");
+		 }
+		 }
+		 
+	}
+
+}
