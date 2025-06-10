9f9fafd08b7daf5cb8a41ec3bd8cbacb13d0670b
Priyankagupta108 <115006768+Priyankagupta108@users.noreply.github.com>
2025-06-10 17:25:41 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,18 @@
+package First;
+
+public class loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i;
+for( i=1;i<=100;i++)
+{
+	if(i%5==0)
+	{
+		continue;
+	}
+	System.out.println(i+" ");
+}
+	}
+
+}
